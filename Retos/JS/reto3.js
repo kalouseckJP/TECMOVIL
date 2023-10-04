9bc51d2d1566a9acacd3371d9f847a0c655c617c
kalouseckJP <juanpablo.rodriguez@alumnos.ulagos.cl>
2023-10-03 23:18:49 -0300

    reto3

--- Retos/JS/reto3.js
+++ Retos/JS/reto3.js
@@ -1,6 +1,25 @@
-const input = document.getElementById('AA')
+const teclaEnter = document.getElementsByClassName("linea1")[0];
+teclaEnter.addEventListener("keyup", ({key})=>{
+    if (key === "Enter"){
+        submit()
+    }
+});
 
-const porcentaje = document.getElementById('selecttest')
-const v = porcentaje
+function submit(){
+    const input = document.getElementById('precioInput').value
+    const porcentaje = document.getElementById('selectPropina').value
+    if (input<=0 || input=='' || /[\d]+[.][\d]+/.test(input) || !/^[\d]+/.test(input)) {
+        document.getElementById('propinaLabel').innerHTML = 'Ingrese un numero mayor a 0 sin decimales'    
+        document.getElementById('totalPagar').style.display = 'none'
+        document.getElementById('propinaLabel').style.display = 'block'
+    }else{
+        document.getElementById('propinaLabel').innerHTML = 'Propina: ' +'$' + calculo(porcentaje,input) + ' CLP'
+        document.getElementById('totalPagar').innerHTML = 'Total a pagar: ' +'$' + (calculo(porcentaje,input)+parseInt(input)) + ' CLP';
+        document.getElementById('propinaLabel').style.display = 'block'
+        document.getElementById('totalPagar').style.display = 'block'
+    }
+};
 
-console.log(v);
+function calculo(porcentaje, input){
+    return Math.trunc(input*porcentaje);
+};
