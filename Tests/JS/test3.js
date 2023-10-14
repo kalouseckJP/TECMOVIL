66c227125e08e81de4dc2e09356d00d078ad2c26
kalouseckJP <juanpablo.rodriguez@alumnos.ulagos.cl>
2023-10-14 14:45:23 -0300

    tests

--- Tests/JS/test3.js
+++ Tests/JS/test3.js
@@ -2,7 +2,7 @@ function divideBy250() {
     while(true) {
         let inputNumber = prompt("Please enter a number (or 'exit' to stop):");
         if(inputNumber.toLowerCase() === 'exit') break;
-        console.log(inputNumber / 250);
+        console.log(inputNumber / 125);
     }
 }
 
